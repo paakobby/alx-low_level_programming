6a3750608ecab459ebfa73917be9787f15d667e2
Kobby Fynn <kobbyfynn88@gmail.com>
2023-04-07 13:42:06 -0700

    solve task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,18 +15,18 @@ int main(int argc, char *argv[])
 	int i;
 	int mul = 1;
 
-	if (argc > 1 && argc < 4)
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mul *= atoi(argv[i]);
 		}
 		printf("%d\n", mul);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
