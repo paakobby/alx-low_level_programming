0fd9b5813fe593d5eeaa6d82292cc475a46393c8
Kobby Fynn <kobbyfynn88@gmail.com>
2023-04-13 17:40:20 -0700

    push c file instead of executable file

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range- function that creates an array of integers
+ *
+ * @min: int
+ *
+ * @max: int
+ *
+ * @Return: a pointer to the newly created array;
+ *  if min > max, return NULL, if malloc fails, return NULL
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = max - min + 1;
+
+	arr = malloc(sizeof(*arr) * size);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = min + i;
+	}
+
+	return (arr);
+}
