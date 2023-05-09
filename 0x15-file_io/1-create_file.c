a0dd7f801b20de3bb73aaaabe493d86f506b5ca1
Kobby Fynn <kobbyfynn88@gmail.com>
2023-05-09 06:47:00 -0700

    redo task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,29 +12,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-
-	int o, w, len;
+	int fd, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-		{
+		while (text_content[len])
 			len++;
-		}
 	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
 	{
 		return (-1);
 	}
-	close(o);
+	if (len > 0)
+	{
+		if (write(fd, text_content, len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
 	return (1);
 }
